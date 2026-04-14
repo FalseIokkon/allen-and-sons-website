81c8a22a3aa0348eb611d5e0519893d1c65b1adf
FalseIokkon <149558736+FalseIokkon@users.noreply.github.com>
2026-04-13 22:00:49 -0700

    Changed all images to .avif and updated image links

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 2026-04-13
+### index.html:
+- changed placeholder email to real mypingponclub@gmail.com
+- added links to email & phone # (clicking the phone # while on mobile phone should automatically open in phone app)
+- 
--- src/javascript/gallery.js
+++ src/javascript/gallery.js
@@ -2,19 +2,19 @@ const galleryGrid = document.getElementById("galleryGrid");
 
 const images = [
   {
-    src: "./images/gallery/gallery-1.jpg",
+    src: "./images/gallery/gallery-1.avif",
     alt: "Cho, Song, Kyaw, and James"
   },
   {
-    src: "./images/gallery/gallery-2.jpg",
+    src: "./images/gallery/gallery-2.avif",
     alt: "2023 Summer Camp (Week 6)"
   },
   {
-    src: "./images/gallery/gallery-3.jpg",
+    src: "./images/gallery/gallery-3.avif",
     alt: "Birthday Party November 2024"
   },
   {
-    src: "./images/gallery/club-play.jpg",
+    src: "./images/gallery/club-play.avif",
     alt: "Weekly Round Robin"
   }
 ];
